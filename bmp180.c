548a0967883a360faf64c72c45bfbf12344841d1
Christoph Gommel <cgommel@me.com>
2015-01-09 00:12:23 +0100

    It works on my cubie

--- bmp180.c
+++ bmp180.c
@@ -48,7 +48,7 @@ int reg_read_byte(int handle, uint8_t devaddr, uint8_t regaddr,
 	messages[1].len = 1;
 
 	if (ioctl(handle, I2C_RDWR, (unsigned long) &iocall) < 0) {
-		printf("error during reg_write_byte()\n");
+		printf("error during reg_read_byte()\n");
 		return -1;
 	}
 	return 0;
@@ -74,7 +74,7 @@ int reg_read_short(int handle, uint8_t devaddr, uint8_t regaddr,
 	messages[1].len = 2;
 
 	if (ioctl(handle, I2C_RDWR, (unsigned long) &iocall) < 0) {
-		printf("error during reg_write_byte()\n");
+		printf("error during reg_read_short()\n");
 		return -1;
 	}
 
@@ -96,7 +96,7 @@ typedef struct {
 	int16_t MD; //=   2868;
 }bmp180_calibration;
 
-#define BMP180_I2C_ADDR (0xAA)
+#define BMP180_I2C_ADDR (0x77)
 
 int bmp180_read_cal (int fd, bmp180_calibration* cal)
 {
@@ -129,7 +129,70 @@ int main(void) {
 	ioctl(fd, I2C_TIMEOUT, 3); // i2c timeout, 1 for 10ms ; if too small, i2c may lose response
 	ioctl(fd, I2C_RETRIES, 3);    // i2c retry limit
 
+	bmp180_calibration cal;
 
+	if(0==bmp180_read_cal(fd,&cal))
+        printf("Calibration read\n",cal.AC1);
+	else
+	{
+		printf("Error reading calibration\n");
+		return -1;
+	}
 
+	int32_t X1;
+   	int32_t X2;
+   	int32_t X3;
+    
+   	int32_t B3;
+   	int32_t B4;
+   	int32_t B5;
+	int32_t B6;
+	int32_t B7;
+	int32_t T;
+	int32_t p;
+   int16_t oss=0;
+   
+
+	reg_write_byte(fd,BMP180_I2C_ADDR,0xf4,0x2e); //Start Temperature Measurement
+	usleep(4500);//Wait for Temperature measurement
+	uint16_t temp16;
+	reg_read_short(fd,BMP180_I2C_ADDR,0xf6,&temp16);
+	int32_t UT=temp16;
+
+	reg_write_byte(fd,BMP180_I2C_ADDR,0xf4,0x34); //Start Pressure Measurement
+	usleep (76500); //Wait for extremly precise pressure measurement
+	reg_read_short(fd,BMP180_I2C_ADDR,0xf6,&temp16);
+	int32_t UP=temp16;
+
+	X1=(UT-cal.AC6)*cal.AC5/(1<<15);
+	X2=cal.MC*(1<<11)/(X1+cal.MD);
+	B5=(X1+X2);
+	T=(B5+8)/(1<<4);
+    printf("Temperature is %i.%i°C\n",T/10,T%10);
+ 
+ B6=B5-4000;
+    X1=(cal.B2*(B6*B6/(1<<12)))/(1<<11);
+    X2=cal.AC2*B6/(1<<11);
+    X3=X1+X2;
+    B3=(((cal.AC1*4+X3)<<oss)+2)/4;
+    X1=cal.AC3*B6/(1<<13);
+    X2=(cal.B1*(B6*B6/(1<<12)))/(1<<16);
+    X3=((X1+X2)+2)/(1<<2);
+    B4=cal.AC4*(uint32_t)(X3+32768)/(1<<15);
+    B7=((uint32_t)UP-B3)*(50000>>oss);
+    // if(B7<0x80000000)
+    //     p=(B7*2)/B4;
+    // else
+    p=(B7/B4)*2;
+    X1=(p/(1<<8))*(p/(1<<8));
+    X1=(X1*3038)/(1<<16);
+    X2=(-7357*p)/(1<<16);
+    p=p+(X1+X2+3791)/(1<<4);
+    
+    printf("Pressure is %i Pa\n",p);
+	 double p0;
+    double alt=125;
+    p0=((double)p)/pow(1-alt/44330.0,5.255);
+    printf("Assuming we are at %.1f m elevation the Pressure at sea level is %0.1f Pa\n",alt,p0);
 	return (0);
 }
